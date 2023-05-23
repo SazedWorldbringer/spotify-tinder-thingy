9b655d0641573d923f40d24a5f7fa4176a228c7e
Atharva Pardeshi <atharva.exe@gmail.com>
2023-05-23 23:27:19 +0530

    fix: remove unwanted imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 import '../styles/globals.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Matches from './pages/match.jsx'
-import ErrorPage from './pages/error.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
