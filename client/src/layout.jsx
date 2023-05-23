5532ab0872fbf8d9bb9e00c0066a8fdaf6ba2b9b
Atharva Pardeshi <atharva.exe@gmail.com>
2023-05-23 23:25:50 +0530

    refactor: move layout code

---
+++ client/src/layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom"
+import Footer from "./components/footer/footer"
+import Nav from "./components/nav/nav"
+
+const Layout = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
+        <Nav />
+      </header>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
