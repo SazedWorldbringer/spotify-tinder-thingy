732980a8d14f339bf7815fbe1d8d976405274880
Atharva Pardeshi <atharva.exe@gmail.com>
2023-05-29 18:06:28 +0530

    feat: transparent background to nav and card

--- client/src/layout.jsx
+++ client/src/layout.jsx
@@ -5,7 +5,7 @@ import Nav from "./components/nav/nav"
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 bg-background">
+      <header className="container z-40 bg-background/30 backdrop-blur-sm">
         <Nav />
       </header>
       <main className="flex-1">
