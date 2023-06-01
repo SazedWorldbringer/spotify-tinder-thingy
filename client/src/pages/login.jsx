8afca98a5151b45342277e97a25e4e6a96ed6a42
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-01 22:40:14 +0530

    feat: try auth with spotify

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,5 +1,14 @@
+import api from "../api/api"
+import { Button } from "../components/ui/button"
+
 export default function Login() {
+  const handleLogin = async () => {
+    api.createAccount()
+  }
   return (
-    <h1>Login</h1>
+    <div>
+      <h1 className="text-3xl">Login</h1>
+      <Button onClick={handleLogin}>Login with Spotify</Button>
+    </div>
   )
 }
