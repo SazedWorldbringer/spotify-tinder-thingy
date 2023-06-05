118b90a672a59d981a65a361438733bee4725e31
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-05 23:17:21 +0530

    feat: update login page, add register page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,45 @@
-import api from "../api/api"
-import { Button } from "../components/ui/button"
+import { Link } from "react-router-dom"
 
-export default function Login() {
-  const handleLogin = async () => {
-    api.createAccount()
-  }
+import { cn } from "../lib/utils"
+import { buttonVariants } from "../components/ui/button"
+import { Icons } from "../components/icons"
+import { UserLoginForm } from "../components/auth/user-login-form"
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1 className="text-3xl">Login</h1>
-      <Button onClick={handleLogin}>Login with Spotify</Button>
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Link
+        to="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <Icons.logo className="mx-auto h-6 w-6" />
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Welcome back
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email to sign in to your account
+          </p>
+        </div>
+        <UserLoginForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          <Link
+            to="/register"
+            className="hover:text-brand underline underline-offset-4"
+          >
+            Don&apos;t have an account? Sign Up
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
---
+++ src/pages/register.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom"
+
+import { cn } from "../lib/utils"
+import { buttonVariants } from "../components/ui/button"
+import { Icons } from "../components/icons"
+import { UserRegisterForm } from "../components/auth/user-register-form"
+
+export const metadata = {
+  title: "Create an account",
+  description: "Create an account to get started.",
+}
+
+export default function RegisterPage() {
+  return (
+    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        to="/login"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Login
+      </Link>
+      <div className="hidden h-full bg-muted lg:block" />
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <Icons.logo className="mx-auto h-6 w-6" />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <UserRegisterForm />
+        </div>
+      </div>
+    </div>
+  )
+}
