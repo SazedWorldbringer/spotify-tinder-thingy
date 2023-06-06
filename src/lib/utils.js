917e73d3a52f9e1d33b418c60c54852828f03d49
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-06 23:07:36 +0530

    fix: move Server config to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
 	return twMerge(clsx(inputs))
 }
+
+export const Server = {
+	spotifyClient: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+	endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
+	project: import.meta.env.VITE_APPWRITE_PROJECT,
+	collectionID: import.meta.env.VITE_COLLECTION_ID,
+	databaseID: import.meta.env.VITE_DATABASE_ID,
+}
--- src/utils/config.js
+++
@@ -1,7 +0,0 @@
-export const Server = {
-	spotifyClient: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-	endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
-	project: import.meta.env.VITE_APPWRITE_PROJECT,
-	collectionID: import.meta.env.VITE_COLLECTION_ID,
-	databaseID: import.meta.env.VITE_DATABASE_ID,
-}
